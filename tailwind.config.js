90e5841fdc62a39ba5e15b58ec1c0468b543eaac
Lukas Van Nuffel <lukasvannuffel02@gmail.com>
2024-12-26 12:26:17 +0100

    Implement tailwindCSS in my project and change styles

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,twig}', // Voor Craft CMS of Twig-bestanden
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
