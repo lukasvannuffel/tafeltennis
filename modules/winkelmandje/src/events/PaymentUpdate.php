27e678a13a95b7c9137eb9bb7f9b4ab0e6f0bc69
Lukas Van Nuffel <lukasvannuffel02@gmail.com>
2025-04-25 12:57:45 +0200

    restore boilerplate code leerkracht

--- modules/winkelmandje/src/events/PaymentUpdate.php
+++ modules/winkelmandje/src/events/PaymentUpdate.php
@@ -88,7 +88,7 @@ class PaymentUpdate {
         // find the stash
         $stash = Entry::find()
             ->section('stash_section')
-            ->id($stashId)
+            ->uid($stashId)
             ->one();
 
         // check if the stash was found
@@ -97,13 +97,10 @@ class PaymentUpdate {
             return;
         }
 
-        // Update the stash status to "paid"
-        $stash->setFieldValue('stash_status', 'paid');
-        
-        // Empty the stash items
-        $stash->setFieldValue('stash_items', []);
-
-        // Save the updated stash
+        // title is in this format: [OPENSTAAND] Stash voor somebody (5 items)
+        // change OPENSTAAND to BETAALD
+        $stash->title = str_replace('OPENSTAAND', 'BETAALD', $stash->title);
+        $stash->stash_status = "paid";
         if (!Craft::$app->elements->saveElement($stash)) {
             Craft::error('Failed to save stash element: ' . $stashId, __METHOD__);
         }
